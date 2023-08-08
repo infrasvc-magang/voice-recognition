a8d8442c1b23e0a6ce7e1c5e6ff496602d83ef07
axar02 <ivannoorazhar@gmail.com>
2023-08-08 13:24:37 +0700

    backend voice fix

--- kerangka/urls.py
+++ kerangka/urls.py
@@ -1,11 +1,9 @@
 from unicodedata import name 
 from django.urls import path
-from . import voice
 from . import views
 
 urlpatterns = [
     path('voice/',views.home,name='voice'),
-    # path('home',views.home,name='home'),
-    path('voiceget/', voice.my_view, name='voiceget'),
-    # path('view/', models.view, name='view'),
+    path('postdata/', views.postdata, name='postdata'),
+    path('datagpt/', views.datagpt, name='datagpt'),
 ]
